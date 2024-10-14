c569c1636cde492072892eb7611b601ba3cbafdc
Leonhard Hennig <leonhard.hennig@dfki.de>
2024-10-14 14:31:56 +0200

    fix 502 bad gateway errors of the external mGENRE service by sleeping for 15 minutes and then retrying

--- src/main/java/org/aksw/gerbil/annotator/impl/neamt/NeamtD2KBAnnotator.java
+++ src/main/java/org/aksw/gerbil/annotator/impl/neamt/NeamtD2KBAnnotator.java
@@ -1,8 +1,10 @@
 package org.aksw.gerbil.annotator.impl.neamt;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.aksw.gerbil.annotator.D2KBAnnotator;
+import org.aksw.gerbil.datatypes.ErrorTypes;
 import org.aksw.gerbil.exceptions.GerbilException;
 import org.aksw.gerbil.transfer.nif.Document;
 import org.aksw.gerbil.transfer.nif.MeaningSpan;
@@ -10,8 +12,12 @@ import org.aksw.gerbil.transfer.nif.Span;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NeamtD2KBAnnotator extends AbstractNeamtAnnotator implements D2KBAnnotator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NeamtD2KBAnnotator.class);
 
     public NeamtD2KBAnnotator(String serviceUrl, String components, String lang) {
         super(serviceUrl, components, lang);
@@ -19,7 +25,22 @@ public class NeamtD2KBAnnotator extends AbstractNeamtAnnotator implements D2KBAn
 
     @Override
     public List<MeaningSpan> performD2KBTask(Document document) throws GerbilException {
-        return request(document).getMarkings(MeaningSpan.class);
+        try {
+            return request(document).getMarkings(MeaningSpan.class);
+        }
+        catch (GerbilException e) {
+            if (e.getMessage().startsWith("Response has the wrong status: HTTP/1.1 502 Bad Gateway")) {
+                try {
+                    LOGGER.info("Retrying failed request in 15 minutes");
+                    TimeUnit.MINUTES.sleep(15);
+                    return request(document).getMarkings(MeaningSpan.class);
+                }
+                catch (InterruptedException ef) {
+                    throw new GerbilException(ef, ErrorTypes.UNEXPECTED_EXCEPTION);
+                }
+            }
+            else throw e;
+        }
     }
 
     @Override
